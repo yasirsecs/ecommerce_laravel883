59f675fc22a7d14867a0129c1287397bde741f03
yasirsecs <114733212+yasirsecs@users.noreply.github.com>
2022-10-17 22:18:53 +0500

    171022 pcard6

--- resources/views/pcard6.blade.php
+++ resources/views/pcard6.blade.php
@@ -8,11 +8,11 @@
 <div id="siteWrapper" class=" main-container container p-5 w-full">
     @include('layouts.mobile-navbar')
                 <div class="  px-5 lg:px-0 flex flex-col lg:flex-row item-center justify-center" >
-                    <div class="w-1/3 bg-red border">
+                    <div class="lg:w-1/3 bg-red border">
                         <div class="item-images grid grid-rows-2 gap-1 w-full">
-                          <div class="large-image-view w-full  border rounded-md ">
+                          <div class="large-image-view w-full  border rounded-md object-center">
                             <a href="#">
-                            <img class=" p-5 max-w-sm " src="{{asset('images/shoe2.png')}}" alt="product image">
+                            <img class=" " src="{{asset('images/shoe2.png')}}" alt="product image">
                             </a>
                           </div>
                           <div class="large-image-list grid grid-cols-4 w-full h-1/5  ">
@@ -37,7 +37,7 @@
                     <!-- second card-->
                     
                     <!--   shoping cart start here --> 
-                    <div class="w-1/4   rounded-lg dark:bg-gray-800 dark:border-gray-700 border  ">
+                    <div class="lg:w-1/4   rounded-lg dark:bg-gray-800 dark:border-gray-700 border p-5  ">
                       <div class="px-5 pb-5">
                         <a href="#">
                             <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">SLEEK TRAINERS INSPIRED BY RUNNING TECHNOLOGY.</h5>
@@ -51,15 +51,19 @@
                             <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                             <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                         </div>
-                        <div class="flex ">
-                          <div class="sizes flex content-evenly ">
-                            <div class="small hover:bg-black hover:text-white text-justify align-middle text-xl w-10 h-10">4</div>
-                            <div class="large hover:bg-black hover:text-white text-justify align-middle text-xl w-10 h-10">5</div>
-                            <div class="extralarge  hover:bg-black hover:text-white text-justify align-middle text-xl w-10 h-10">6</div>
+                        <div class="flex flex-col">
+                          <div class="sizes flex flex-row p-5 ">
+                            <div class="small hover:bg-black hover:text-white  text-xl basis-1/4 text-center align-middle  ">4</div>
+                            <div class="large hover:bg-black hover:text-white  text-xl basis-1/4 text-center align-middle ">5</div>
+                            <div class="extralarge  hover:bg-black hover:text-white  text-xl basis-1/4 text-center align-middle  ">6</div>
+                            <div class="extralarge  hover:bg-black hover:text-white  text-xl basis-1/4 text-center align-middle ">7</div>
+                    
                           </div>
-                          <div class="price"><span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span></div>
-                            
+                          <div class="price flex justify-between"><span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
                             <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                          </div>
+                            
+                            
                         </div>
                     </div>   
                     </div>
